5bfb492924b557187f033b5c74b6306978271b2e
Hank Lo <Hlo1453@gmail.com>
2016-02-17 12:11:55 -0800

    Fixed Follow Camera Script

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -7,17 +7,19 @@ Makes Camera.main (main Camera) follow whatever target is attached (gonna be use
 */
 
 public class FollowCamera : MonoBehaviour {
-	//Amount it slows down, also known as damp time.
-    public float friction = 0.15f;
-    private Vector3 velocity = Vector3.zero;
-    public Transform target;
+    public float cameraDistOffset = 10;
+    private Camera mainCamera;
+    private GameObject player;
+
+    // Use this for initialization
+    void Start () {
+        mainCamera = GetComponent<Camera>();
+        player = GameObject.Find("Wall1");
+    }
 
     // Update is called once per frame
-    void FixedUpdate() {
-        //Point in the world the mouse is at
-        Vector3 point = Camera.main.WorldToViewportPoint(target.position);
-        Vector3 delta = target.position - Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
-        Vector3 destination = transform.position + delta;
-        transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, friction);
+    void Update () {
+        Vector3 playerInfo = player.transform.transform.position;
+        mainCamera.transform.position = new Vector3(playerInfo.x, playerInfo.y, playerInfo.z - cameraDistOffset);
     }
 }
